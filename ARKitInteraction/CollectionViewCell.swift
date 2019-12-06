b3365449a88ddd728ba2e43c7ba7c2acb594d5c6
vivian-he <vyang44@gmail.com>
2019-12-06 18:25:30 -0500

    hi

---
+++ ARKitInteraction/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ARKitInteraction
+//
+//  Created by Vivian on 12/6/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ARKitInteraction/global.swift
@@ -0,0 +1,9 @@
+//
+//  global.swift
+//  ARKitInteraction
+//
+//  Created by Vivian on 12/6/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
